7ea2da6691fdae7fd48157a6fa2a871a3ee59c4b
Julien Linzas <linzas.julien@gmail.com>
2020-11-20 23:15:45 +0100

    reupdate

--- app/controllers/doses_controller.rb
+++ app/controllers/doses_controller.rb
@@ -3,6 +3,8 @@ class DosesController < ApplicationController
     @cocktail = Cocktail.find(params[:cocktail_id])
     @dose = Dose.new
   end
+
+
   def create
     @dose = Dose.new(dose_params)
     @cocktail = Cocktail.find(params[:cocktail_id])
